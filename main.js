88d3d19bc0d0745293b338630b75a85fe25c2475
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2018-11-27 14:42:30 -0300

    adicionei o localstorage

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+//local reservado para variáveis globais
+var ind
+
+
 var bSave = document.querySelector(".btnSave"); // . é para classe, # é para ID
 bSave.addEventListener("click", function(event){
 	event.preventDefault();
     console.log(nota.value);
+    localStorage.setItem("nota", nota.value);
+    ind = localStorage.length + 1;
+	localStorage.setItem("nota" + ind, nota.value);
 });
 
