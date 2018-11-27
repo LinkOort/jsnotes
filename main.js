d82926205ff10204fe682789373810a61ea89903
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2018-11-27 14:33:20 -0300

    adicionando função e métodos ao botão SAVE

---
+++ main.js
@@ -0,0 +1,6 @@
+var bSave = document.querySelector(".btnSave"); // . é para classe, # é para ID
+bSave.addEventListener("click", function(event){
+	event.preventDefault();
+    console.log(nota.value);
+});
+
